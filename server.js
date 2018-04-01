5c5e1adc4029175843e5bd9c22d7b886d54009a8
kassadube <noamsrb@yahoo.com>
2018-04-02 02:49:51 +0300

    add caching still not working ...

--- server.js
+++ server.js
@@ -1,13 +1,40 @@
 import express from 'express';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import compression from 'compression';
 import bodyParser from 'body-parser';
+import { Engine } from 'apollo-engine';
+
 import schema from './data/schema';
 
 const GRAPHQL_PORT = 3005;
+const ENGINE_API_KEY = 'service:kassadube-6004:ZODhWRsOh9wMJTH1V18ZjA'; // TODO
+
+const engine = new Engine({
+  engineConfig: {
+    apiKey: ENGINE_API_KEY,
+    stores: [
+      {
+        name: 'inMemEmbeddedCache',
+        inMemory: {
+          cacheSize: 20971520 // 20 MB
+        }
+      }
+    ],
+    queryCache: {
+      publicFullQueryStore: 'inMemEmbeddedCache'
+    }
+  },
+  graphqlPort: GRAPHQL_PORT
+});
+
+engine.start();
 
 const graphQLServer = express();
 
-graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+graphQLServer.use(engine.expressMiddleware());
+graphQLServer.use(compression());
+
+graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({ schema, tracing: true }));
 graphQLServer.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
 graphQLServer.listen(GRAPHQL_PORT, () =>
